7a948e00cbcfe18a3e3167153b3a2ae2f51486f2
jessebyun <139979470+jessebyun@users.noreply.github.com>
2023-10-29 22:05:55 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    # answer_state is title-cased
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name?").title()
+
+    if answer_state == "Exit":
+        missed_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missed_states.append(state)
+        df = pandas.DataFrame(missed_states)
+        df.to_csv("states_to_learn")
+        break
+    if answer_state in all_states and answer_state not in guessed_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+# turtle.onscreenclick(get_mouse_click_coor)
+# turtle.mainloop()
+# screen.exitonclick()
